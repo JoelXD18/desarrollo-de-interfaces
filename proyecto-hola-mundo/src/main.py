59cce41bc388c9b9a21fd1af8706b6f75a808e14
Joel Ramos <96124649+JoelXD18@users.noreply.github.com>
2025-10-14 13:36:49 +0200

    Update main.py

--- proyecto-hola-mundo/src/main.py
+++ proyecto-hola-mundo/src/main.py
@@ -1,1 +1,15 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from ventana import Ventana
 
+# creo la app, si no no funciona nada
+app = QApplication(sys.argv)
+
+# creo mi ventana
+ventana = Ventana()
+
+# la muestro en pantalla
+ventana.show()
+
+# esto es para que la app siga abierta hasta que la cierre
+sys.exit(app.exec())
